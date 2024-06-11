9961dfd6f63e89bb72bb06d462a654c2f18b2f2d
+ <+>
2024-06-10 21:42:47 -0400

    DOM Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+ main.remove()
+
+ const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+ 
+document.getElementById("h1");
+ newHeader.textContent = "Cody is the champion"
